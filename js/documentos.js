0d85d59af1058731d6b3f4c051ca5dd3e2aed3f9
Anderson Evangelista <andersonevangelista1@gmail.com>
2020-01-27 21:41:53 -0300

    Base do site de consulta de documentos no Firebase

---
+++ js/documentos.js
@@ -0,0 +1,14 @@
+function search(){
+    var cpf = document.getElementById('inputCPF').value
+    validation(cpf);
+}
+function validation(cpfValue){
+    document.getElementById('busca').setAttribute("class", "ocultar");
+    document.getElementById('resultado').setAttribute("class", "ocultar");
+    document.getElementById('tituloDocumentos').innerHTML = 'Certificados de:'+cpfValue;
+}
+function back(){
+    document.getElementById('busca').removeAttribute("class", "ocultar");
+    document.getElementById('resultado').setAttribute("class", "ocultar");
+    document.getElementById('inputCPF').value = '';
+}
---
+++ js/initFirebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCDvBvoIboRAYaex9Fuyp5GYZVBuOBFfh8",
+    authDomain: "documentos-f4b75.firebaseapp.com",
+    databaseURL: "https://documentos-f4b75.firebaseio.com",
+    projectId: "documentos-f4b75",
+    storageBucket: "documentos-f4b75.appspot.com",
+    messagingSenderId: "402725327808",
+    appId: "1:402725327808:web:785df4666ace2a28dfee25"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
