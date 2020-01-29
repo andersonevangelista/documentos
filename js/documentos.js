c74c816b8d49ab1e66cee67a01dd6591edec4fba
Anderson Evangelista <andersonevangelista1@gmail.com>
2020-01-28 21:46:41 -0300

    Busca da pasta por CPF e arquivo no Storage.

--- js/documentos.js
+++ js/documentos.js
@@ -4,9 +4,48 @@ function search(){
 }
 function validation(cpfValue){
     document.getElementById('busca').setAttribute("class", "ocultar");
-    document.getElementById('resultado').setAttribute("class", "ocultar");
+    document.getElementById('resultado').removeAttribute("class", "ocultar");
     document.getElementById('tituloDocumentos').innerHTML = 'Certificados de:'+cpfValue;
+
+    var storage = firebase.storage();
+    //Retorna uma promisses que será processada
+    storage.ref().child(cpfValue).listAll().then(function(todosArquivos){
+        if(todosArquivos.items.length >=1){
+            listFiles(cpfValue);
+            next(cpfValue);
+        } else{
+            alert('CPF não cadastrado');    
+        }
+
+    }).catch(function(error){
+        console.log('ERRO', error);
+    });
+           
 }
+function listFiles(cpfValue){
+    document.getElementById('tituloDocumentos').innerHTML = 'Certificados de:'+cpfValue;
+    var storage = firebase.storage();
+    var arquivos;
+    var nomeArquivos = [];
+    var linksArquivos = [];
+    storage.ref().child(cpfValue).listAll().then(function(todosArquivos){
+        arquivos = todosArquivos.items;
+        console.log(arquivos);
+        for(let i=0; i<arquivos.length; i++){
+            nomeArquivos.push(arquivos[i].name);
+            storage.ref(cpfValue+ '/'+nomeArquivos[i]).getDownloadURL().then(function(url){
+                console.log(url);
+            });
+        }
+    });
+}
+
+function next(cpfValue){
+    document.getElementById('busca').setAttribute("class", "ocultar");
+    document.getElementById('resultado').removeAttribute("class", "ocultar");
+    
+}
+
 function back(){
     document.getElementById('busca').removeAttribute("class", "ocultar");
     document.getElementById('resultado').setAttribute("class", "ocultar");
